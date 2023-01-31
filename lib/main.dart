ee0e83975caf5c96a2a35dc9db3a44cebd058302
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-31 16:47:23 +0100

    Convert to Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  String title = 'Tap the screen'; 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = 'Tap the screen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(title),
+        title: Text(title),
       ),
       body: GestureDetector(
-      child: Container(
-        color: Colors.white, 
-        child:Text('Tap Me'),
-        ), 
+        onTap: () {
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        },
+        child: Container(
+          color: Colors.white,
+          child: const Text('Tap Me'),
+        ),
+      ),
     );
   }
 }
